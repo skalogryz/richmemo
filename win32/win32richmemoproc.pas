71b0b5da0faa149b9e420ee5368895f36d4b356c
Dmitry Boyarintsev <16345231+skalogryz@users.noreply.github.com>
2009-06-20 20:54:09 +0000

    added license information, changed package version to 1.0 and a small cleanup

--- win32/win32richmemoproc.pas
+++ win32/win32richmemoproc.pas
@@ -1,3 +1,22 @@
+{
+ win32richmemoproc.pas 
+ 
+ Author: Dmitry 'skalogryz' Boyarintsev 
+
+ *****************************************************************************
+ *                                                                           *
+ *  This file is part of the Lazarus Component Library (LCL)                 *
+ *                                                                           *
+ *  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
+ *  for details about the copyright.                                         *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+ *                                                                           *
+ *****************************************************************************
+}
+
 unit Win32RichMemoProc; 
 
 {$mode objfpc}{$H+}
@@ -147,7 +166,6 @@ class function TRichEditManager.GetSelectedTextStyle(RichEditWnd: Handle;
 var
   w     : WPARAM;
   fmt   : TCHARFORMAT;
-  mask  : LongWord;
   
 begin
   Result := false;

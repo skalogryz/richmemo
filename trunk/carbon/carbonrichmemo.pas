081257ebb99c0257567b35ac15ed9932a4b67afa
skalogryz <skalogryz@42e0a123-9135-4a3e-8913-9b69458bb2e1>
2009-06-20 20:54:09 +0000

    added license information, changed package version to 1.0 and a small cleanup

--- trunk/carbon/carbonrichmemo.pas
+++ trunk/carbon/carbonrichmemo.pas
@@ -1,3 +1,22 @@
+{
+ carbonrichmemo.pas 
+ 
+ Author: Dmitry 'skalogryz' Boyarintsev 
+
+ *****************************************************************************
+ *                                                                           *
+ *  This file is part of the Lazarus Component Library (LCL)                 *
+ *                                                                           *
+ *  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
+ *  for details about the copyright.                                         *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+ *                                                                           *
+ *****************************************************************************
+}
+
 unit CarbonRichMemo;
 
 {$mode objfpc}{$H+}

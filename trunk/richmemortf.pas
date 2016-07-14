298582d0fae73899d72ab0fd75f23c1060a03d09
wp_xxyyzz <wp_xxyyzz@42e0a123-9135-4a3e-8913-9b69458bb2e1>
2016-07-14 22:43:59 +0000

    richmemo: Fix compilation with Laz trunk

--- trunk/richmemortf.pas
+++ trunk/richmemortf.pas
@@ -5,7 +5,7 @@ interface
 {$mode objfpc}{$h+}
 
 uses
-  Classes, SysUtils, LCLProc, LCLIntf, LConvEncoding, Graphics,
+  Classes, SysUtils, LCLProc, LCLIntf, LConvEncoding, LazUTF8, Graphics,
   RichMemo, RTFParsPre211;
 
 function MVCParserLoadStream(ARich: TCustomRichMemo; Source: TStream): Boolean;

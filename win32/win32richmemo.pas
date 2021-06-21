c8a070e343ee306a34e2bd55c40d31f427380025
paweld <biuro@paweld.it>
2021-06-21 07:48:58 +0200

    fix compile on fpc 3.2.2

--- win32/win32richmemo.pas
+++ win32/win32richmemo.pas
@@ -1639,7 +1639,7 @@ type
   end;
   PStreamText = ^TStreamText;
 
-{$IF FPC_FULLVERSION>=30301}
+{$IF FPC_FULLVERSION>=30202}
 function Read(dwCookie:DWORD_PTR; pbBuff:LPBYTE; cb:LONG; var pcb:LONG):DWORD; stdcall;
 {$ELSE}
 function Read(dwCookie:PDWORD; pbBuff:LPBYTE; cb:LONG; var pcb:LONG):DWORD; stdcall;
